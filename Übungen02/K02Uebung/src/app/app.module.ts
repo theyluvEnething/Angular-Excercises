525c9a537d2fadb5ccc9bbb05f821a7f52510904
Enething <115583828+theyluvEnething@users.noreply.github.com>
2025-01-20 15:43:28 +0100

    Created Project for Übungen 2 and imported all Angular Materials and Flex Layout

---
+++ Übungen02/K02Uebung/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WeatherService } from 'src/shared/weather-service';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+// Flex Layout Module
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule
+  ],
+  providers: [
+    WeatherService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
