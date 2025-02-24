dd9d7f4897fe19135b47f6523d8a5d5d0e8a2433
Enething <115583828+theyluvEnething@users.noreply.github.com>
2025-02-24 16:53:29 +0100

    Implemented significant part of the BookStoreServiceTest Component

--- Übungen03/K03UebungA/src/app/app.module.ts
+++ Übungen03/K03UebungA/src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BookStoreServiceTestComponent } from './book-store-service-test/book-store-service-test.component';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +22,15 @@ import { BookStoreServiceTestComponent } from './book-store-service-test/book-st
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule,
+
+    MatDividerModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Übungen03/K03UebungA/src/app/book-store-service-test/book-store-service-test.component.ts
+++ Übungen03/K03UebungA/src/app/book-store-service-test/book-store-service-test.component.ts
@@ -1,10 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookStoreService } from '../services/book-store.service';
+import { Book } from 'src/shared/book';
+import { FormGroup } from '@angular/forms';
+import { BookFactory } from 'src/shared/book-factory';
 
 @Component({
   selector: 'ua-book-store-service-test',
   templateUrl: './book-store-service-test.component.html',
   styleUrls: ['./book-store-service-test.component.scss']
 })
-export class BookStoreServiceTestComponent {  
+export class BookStoreServiceTestComponent implements OnInit {
 
+  BookFactory = BookFactory;
+  lastCreatedBookIsbn: string = "";
+  response: any;
+
+  constructor(private bookStoreService: BookStoreService) {}
+
+  ngOnInit() {}
+
+
+  resetStore(): void {
+    this.bookStoreService.resetStore().subscribe(res => {
+      this.response = res
+    }, err =>  {
+      console.log(err)
+      this.response = err
+  });
+    
+  }
+
+  deleteBook(isbn: string): void {
+    this.bookStoreService.deleteBook(isbn).subscribe(res => {
+      this.response = res
+    }, err => {
+      console.log(err)
+      this.response = err
+    })
+  }
+
+  getAllBooks(): Book[] {
+    this.bookStoreService.getAllBooks().subscribe(res => {
+      this.response = res
+    }, err => {
+      console.log(err)
+      this.response = err
+    });
+    return []
+  }
+
+  getBook(isbn: string): void {
+    this.bookStoreService.getBook(isbn).subscribe(res => {
+      this.response = res
+    }, err => {
+      console.log(err)
+      this.response = err
+    });
+  }
+
+  getAllBooksSearchTerm(searchTerm: string): void {
+    this.bookStoreService.getAllBooksSearchTerm(searchTerm).subscribe(res => {
+      this.response = res
+    }, err => {
+      console.log(err)
+      this.response = err
+    });
+  }
+
+  createBook(book: Book): void {
+    this.bookStoreService.createBook(book).subscribe(res => {
+      this.lastCreatedBookIsbn = book.isbn
+      this.response = res
+    }, err => {
+      console.log(err)
+      this.response = err
+    });
+  } 
+
+  
+  createBookIsbn(): void {
+    let randomBook = BookFactory.random()
+    randomBook.isbn = this.lastCreatedBookIsbn;
+
+    this.bookStoreService.createBook(randomBook).subscribe(res => {
+      this.response = res
+    }, err => {
+      console.log(err)
+      this.response = err
+    });
+  } 
+
+
+
+  updateBook(book: Book): void {
+    this.bookStoreService.updateBook(book).subscribe(res => {
+      this.response = res
+    }, err => {
+      console.log(err)
+      this.response = err
+    });
+  }
+
+  checkBook(isbn: string): void {
+    this.bookStoreService.checkBook(isbn).subscribe(res => {
+      this.response = res
+    }, err => {
+      console.log(err)
+      this.response = err
+    });
+  }
 }
